bf056c3f62ce104d9774101f889f6857d5bed413
Monir <moniru005@gmail.com>
2024-06-04 03:18:42 +0600

    product.models and product.validation created

---
+++ src/app/modules/product/product.validation.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod'
+
+const VariantSchema = z.object({
+  type: z.string(),
+  value: z.string(),
+})
+
+const InventorySchema = z.object({
+  quantity: z.number(),
+  inStock: z.boolean(),
+})
+
+const ProductSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  tags: z.array(z.string()),
+  variants: z.array(VariantSchema),
+  inventory: InventorySchema,
+})
+
+export default ProductSchema
